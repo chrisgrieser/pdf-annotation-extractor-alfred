1e559850e259297d79991d37db65bbe85e073dc2
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2021-07-27 17:10:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ pip install pdfminer.six
 
 #install pdfannots
 curl -s -L https://github.com/0xabu/pdfannots/archive/refs/heads/master.zip -o $HOME/Downloads/pdfannots_install.zip
-mkdir -p '/usr/local/lib/python3.9/site-packages/pdfannots-master/'
+mkdir -p /usr/local/lib/python3.9/site-packages/pdfannots-master/
 unzip -q -o $HOME/Downloads/pdfannots_install.zip -d /usr/local/lib/python3.9/site-packages/
 rm $HOME/Downloads/pdfannots_install.zip
 temp=$PWD
-cd '/usr/local/lib/python3.9/site-packages/pdfannots-master/'
+cd /usr/local/lib/python3.9/site-packages/pdfannots-master/
 python3 setup.py install
 cd $temp
 echo 'pdf-annots installation finished'
