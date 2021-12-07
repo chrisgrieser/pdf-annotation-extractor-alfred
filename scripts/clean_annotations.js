5353197e3484da7cd073555a6ff91cfaa9b08656
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-07 18:04:19 +0100

    eslint cleaning scripts

--- scripts/clean_annotations.js
+++ scripts/clean_annotations.js
@@ -1,55 +1,56 @@
 #!/usr/bin/env osascript -l JavaScript
 function run(argv) {
 	ObjC.import("stdlib");
-	let annotations = argv.join('');
+	let annotations = argv.join("");
 
-	//prior cleaning
+	// prior cleaning
 	annotations = annotations
-		.replace(/ {2,}/g, " ") //multiple spaces
-		.replace(/\n{2,}/g, "\n") //empty lines
-		.replaceAll('"',"'")
-		.replaceAll('„',"'")
-		.replaceAll('...',"…")
-		.replaceAll('. . .',"…")
-		.replace("## Detailed comments\n", ""); //remove heading
+		.replace(/ {2,}/g, " ") // multiple spaces
+		.replace(/\n{2,}/g, "\n") // empty lines
+		.replaceAll("\"", "'")
+		.replaceAll("„", "'")
+		.replaceAll("...", "…")
+		.replaceAll(". . .", "…")
+		.replaceAll(" Image", "") // bad scans
+		.replace("## Detailed comments\n", ""); // remove heading
 
-	//import Alfred variables
-	let pandoc_cite;
+	// import Alfred variables
+	let pandocCite;
 	const firstPageNo = parseInt($.getenv("first_page_no")) - 1;
-	const hasBibtexEntry = $.getenv("citekey_insertion") != "no_bibliography_extraction";
+	const hasBibtexEntry = $.getenv("citekey_insertion") !== "no_bibliography_extraction";
 
-	if (hasBibtexEntry) pandoc_cite = "@" + $.getenv("citekey") + ", ";
-	else pandoc_cite = "";
+	if (hasBibtexEntry) pandocCite = "@" + $.getenv("citekey") + ", ";
+	else pandocCite = "";
 
 	// reformat pdfannots' output & insert proper numbers
 	annotations = annotations
 		// re-format free comments
 		.replace(
-			/ \* Page #?(\d+).*?:[\n| ]?([^\*>]+)(?=\n)/gm,
-			"- *$2 [" + pandoc_cite + "p. $1]*"
+			/ \* Page #?(\d+).*?:[\n| ]?([^*>]+)(?=\n)/gm,
+			"- *$2 [" + pandocCite + "p. $1]*"
 		)
 		// re-format commented highlights; lookahead ensures recognition of multi-line-comments
-		// in face of another highlight, document-end, or an already reformated free comment
+		// in face of another highlight, document-end, or an already re-formatted free comment
 		.replace(
 			/ \* Page #?(\d+).*?:\n +> +(.*?)\n (.*?)(?=\n-|\n \*|\n$)/gs,
-			'- __$3:__ "$2" [' + pandoc_cite + 'p. $1]'
+			"- __$3:__ \"$2\" [" + pandocCite + "p. $1]"
 		)
 		// reformat multi-line-highlights properly
 		.replace(
 			/- __[^"]*\n[^"]*:__/gm,
-			function (ml){
+			function (ml) {
 				let output = "";
-				let lines = ml.slice(4,-3).split("\n");
-				lines.forEach(line =>{
+				const lines = ml.slice(4, -3).split("\n");
+				lines.forEach(line => {
 					output += "- __" + line.trim() + "__\n";
 				});
 				return output + "-";
 			}
 		)
-		//insert correct page numbers based on https://stackoverflow.com/a/32664436
+		// insert correct page numbers based on https://stackoverflow.com/a/32664436
 		.replace(
-		   /p\. (\d+)(?=\])/g,
-		   function (match, n) {
+			/p\. (\d+)(?=\])/g,
+			function (match, n) {
 				return "p. " + (parseInt(n) + firstPageNo);
 			}
 		);
@@ -60,82 +61,82 @@ function run(argv) {
 	// "+" and "++" (highlights)
 	// merge quotes with preceding quote
 	annotations = annotations
-		//"+": highlights on one page
+		// "+": highlights on one page
 		.replace(
 			/" \[.*p\. (\d+)\]\n- __\+:__ "(.*?)" \[.*p\. /g,
-			" (…)" + ' $2" [' + pandoc_cite + 'p. $1-'
+			" (…) $2\" [" + pandocCite + "p. $1-"
 		)
-		//"++": highlights spanning two page
+		// "++": highlights spanning two page
 		.replace(
 			/" \[.*p\. (\d+)\]\n- __\+\+:__ "(.*?)" \[.*p\. /g,
-			' $2" [' + pandoc_cite + 'p. $1-'
+			" $2\" [" + pandocCite + "p. $1-"
 		)
-		//corrects page number for quotes merged on the same page
+		// corrects page number for quotes merged on the same page
 		.replace(/p\. (\d+)-(\1)\]/g, "p. $1]");
 
 	// "## " (free comments & highlights)
 	// make into heading
 	annotations = annotations
 		.replace(/- \*(#+) (.*?)\[.*?]\*/g, "\n$1 $2") // free comments
-		.replace(/- __(#+) ?:__ "(.*?)".*?\]/g, "\n$1 $2"); //highlights
+		.replace(/- __(#+) ?:__ "(.*?)".*?\]/g, "\n$1 $2"); // highlights
 
 	// "---" (free comments) becomes hr line
 	annotations = annotations
 		.replace(/- \*--- ?\[.*?]\*/g, "\n---\n"); // free comments
 
 	// "?" (free comments)
-	//make h6 & move up top (i.e., Pseudo-Admonitions)
-	let introRE = new RegExp(/- \*\? ?(.*?)\[.*?]\*/, 'g');
+	// make h6 & move up top (i.e., Pseudo-Admonitions)
+	const introRE = new RegExp(/- \*\? ?(.*?)\[.*?]\*/, "g");
 	let intros = annotations.match(introRE);
-	if (intros != null) {
-		intros = intros.map (item => item.replace(introRE,"###### $1"));
+	if (intros) {
+		intros = intros.map (item => item.replace(introRE, "###### $1"));
 		annotations = annotations
 			.replace(introRE, "ZZZZ")
-			.replaceAll("\nZZZZ","");
+			.replaceAll("\nZZZZ", "");
 		annotations = intros.join("\n") + "\n\n" + annotations;
 	}
 
-	//"X " (free comments & highlights)
-	//make to do item & move up
-	let new_tasks = [];
+	// "X " (free comments & highlights)
+	// make to do item & move up
+	const newTasks = [];
 
-	//free comments
-	let tasksRE = new RegExp(/- \*X ?(.*?)\[.*?]\*/, 'gi');
-	let tasks_fc = annotations.match(tasksRE);
-	if (tasks_fc != null) {
-		tasks_fc = tasks_fc.map (item => item.replace(tasksRE,"- [ ] $1"));
+	// free comments
+	let tasksRE = new RegExp(/- \*X ?(.*?)\[.*?]\*/, "gi");
+	let tasksFc = annotations.match(tasksRE);
+	if (tasksFc) {
+		tasksFc = tasksFc.map (item => item.replace(tasksRE, "- [ ] $1"));
 		annotations = annotations
 			.replace(tasksRE, "ZZZZ")
-			.replaceAll("\nZZZZ","");
-		new_tasks.push(...tasks_fc);
+			.replaceAll("\nZZZZ", "");
+		newTasks.push(...tasksFc);
 	}
-	//highlights
-	tasksRE = new RegExp(/- __X ?:__ "(.*?)".*?\]/, 'gi');
-	let tasks_hl = annotations.match(tasksRE);
-	if (tasks_hl != null) {
-		tasks_hl = tasks_hl.map (item => item.replace(tasksRE,'- [ ] "$1"'));
+	// highlights
+	tasksRE = new RegExp(/- __X ?:__ "(.*?)".*?\]/, "gi");
+	let tasksHl = annotations.match(tasksRE);
+	if (tasksHl) {
+		tasksHl = tasksHl.map (item => item.replace(tasksRE, "- [ ] \"$1\""));
 		annotations = annotations
 			.replace(tasksRE, "ZZZZ")
-			.replaceAll("\nZZZZ","");
-		new_tasks.push(...tasks_hl);
+			.replaceAll("\nZZZZ", "");
+		newTasks.push(...tasksHl);
 	}
 
-	//merge into task section
-	if (new_tasks.length != 0){
+	// merge into task section
+	if (newTasks.length) {
 		annotations =
-			new_tasks.join("\n") + "\n"
+			newTasks.join("\n") + "\n"
 			+ "\n---\n"
 			+ annotations;
 	}
 
-	//"!1 " (free comment)
+	// "!1 " (free comment)
 	// Insert 1st (2nd,...) image and gives it the rest of the comment as label
-	//Obsidian: inserts `![[]]`, else: inserts numbered marker
-	let imageRE = new RegExp (/- \*!(\d+) ?(.*?) ?\[.*\*/, "g");
-	if ($.getenv("output_style") == "obsidian"){
+	// Obsidian: inserts `![[]]`, else: inserts numbered marker
+	const imageRE = new RegExp (/- \*!(\d+) ?(.*?) ?\[.*\*/, "g");
+	if ($.getenv("output_style") === "obsidian") {
 		let filename;
 		if (hasBibtexEntry) filename = $.getenv("citekey");
-		else filename = (new Date()).toISOString().slice(0,10);
+		else filename = (new Date()).toISOString().slice(0, 10);
 
 		annotations = annotations.replace(imageRE, "\n![[" + filename + "_image$1.png|$2]]\n");
 	} else {
@@ -143,53 +144,52 @@ function run(argv) {
 	}
 
 	// "=" (free comments & highlights)
-	//becomes a keyword (tag in the yaml)
-	let new_keywords = [];
+	// becomes a keyword (tag in the yaml)
+	let newKeywords = [];
 
-	//free comments
-	let keywordRE = new RegExp(/- \*= ?(.*?)\[.*?]\*/, 'g');
-	let keyword_fc = annotations.match(keywordRE);
-	if (keyword_fc != null) {
-		keyword_fc = keyword_fc.map (item => item.replace(keywordRE,"$1"));
+	// free comments
+	let keywordRE = new RegExp(/- \*= ?(.*?)\[.*?]\*/, "g");
+	let keywordFc = annotations.match(keywordRE);
+	if (keywordFc) {
+		keywordFc = keywordFc.map (item => item.replace(keywordRE, "$1"));
 		annotations = annotations
 			.replace(keywordRE, "ZZZZ")
-			.replaceAll("\nZZZZ","");
-		new_keywords.push(...keyword_fc);
+			.replaceAll("\nZZZZ", "");
+		newKeywords.push(...keywordFc);
 	}
-	//highlights
-	keywordRE = new RegExp(/- __= ?:__ "(.*?)".*?\]/, 'g');
-	let keyword_hl = annotations.match(keywordRE);
-	if (keyword_hl != null) {
-		keyword_hl = keyword_hl.map (item => item.replace(keywordRE,"$1"));
+	// highlights
+	keywordRE = new RegExp(/- __= ?:__ "(.*?)".*?\]/, "g");
+	let keywordHl = annotations.match(keywordRE);
+	if (keywordHl) {
+		keywordHl = keywordHl.map (item => item.replace(keywordRE, "$1"));
 		annotations = annotations
 			.replace(keywordRE, "ZZZZ")
-			.replaceAll("\nZZZZ","");
-		new_keywords.push(...keyword_hl);
+			.replaceAll("\nZZZZ", "");
+		newKeywords.push(...keywordHl);
 	}
-	let new_value = "";
-	if (new_keywords.length != 0){
-		new_keywords =
-			[... new Set (new_keywords)] //unique only
-			.map (kw => kw.trim().replaceAll(" ","-"));
-		new_value = $.getenv("keywords") + ", " + new_keywords.join(", ");
+	let newValue = "";
+	if (newKeywords.length) {
+		newKeywords =
+			[... new Set (newKeywords)] // unique only
+				.map (kw => kw.trim().replaceAll(" ", "-"));
+		newValue = $.getenv("keywords") + ", " + newKeywords.join(", ");
 	} else {
-		new_value = $.getenv("keywords");
+		newValue = $.getenv("keywords");
 	}
 	// Variablenname für tags muss gewechselt werden, weil Alfred
 	// sich sonst weigert diese zu überschreiben...
-	Application('com.runningwithcrayons.Alfred').setConfiguration
- 	('tags', {
-		toValue: new_value,
-		inWorkflow: $.getenv('alfred_workflow_bundleid'),
-		exportable: false}
-	);
+	Application("com.runningwithcrayons.Alfred").setConfiguration ("tags", {
+		toValue: newValue,
+		inWorkflow: $.getenv("alfred_workflow_bundleid"),
+		exportable: false
+	});
 
 
-	//fix for Annotations beginning with `#tags` (e.g. Annotation Tags)
+	// fix for Annotations beginning with `#tags` (e.g. Annotation Tags)
 	annotations = annotations
-		.replace(/__(#\w+):__/g,"$1") //annotations-tag only (highlight)
-		.replace(/__(#\w+)\b */g,"$1 __") //annotation-tag followed by text (highlight)
-		.replace(/\*(#\w+)\b */g,"$1 *"); //annotation-tag followed by text (free comment)
+		.replace(/__(#\w+):__/g, "$1") // annotations-tag only (highlight)
+		.replace(/__(#\w+)\b */g, "$1 __") // annotation-tag followed by text (highlight)
+		.replace(/\*(#\w+)\b */g, "$1 *"); // annotation-tag followed by text (free comment)
 	
 	return annotations;
 }
--- scripts/clean_underline_annotations.js
+++ scripts/clean_underline_annotations.js
@@ -1,71 +1,72 @@
 #!/usr/bin/env osascript -l JavaScript
 function run(argv) {
 	ObjC.import("stdlib");
-	let annotations = argv.join('');
+	let annotations = argv.join("");
 
-	//prior cleaning
+	// prior cleaning
 	annotations = annotations
-		.replace(/ {2,}/g, " ") //multiple spaces
-		.replace(/\n{2,}/g, "\n") //empty lines
-		.replaceAll('"',"'")
-		.replaceAll('„',"'")
-		.replaceAll('...',"…")
-		.replaceAll('. . .',"…")
-		.replace("## Nits\n", ""); //remove heading
+		.replace(/ {2,}/g, " ") // multiple spaces
+		.replace(/\n{2,}/g, "\n") // empty lines
+		.replaceAll("\"", "'")
+		.replaceAll("„", "'")
+		.replaceAll("...", "…")
+		.replaceAll(". . .", "…")
+		.replaceAll(" Image", "") // bad scans
+		.replace("## Nits\n", ""); // remove heading
 
-	//import Alfred variables
-	let pandoc_cite;
+	// import Alfred variables
+	let pandocCite;
 	const firstPageNo = parseInt($.getenv("first_page_no")) - 1;
-	const hasBibtexEntry = $.getenv("citekey_insertion") != "no_bibliography_extraction";
+	const hasBibtexEntry = $.getenv("citekey_insertion") !== "no_bibliography_extraction";
 
-	if (hasBibtexEntry) pandoc_cite = "@" + $.getenv("citekey") + ", ";
-	else pandoc_cite = "";
+	if (hasBibtexEntry) pandocCite = "@" + $.getenv("citekey") + ", ";
+	else pandocCite = "";
 
 	// reformat pdfannots' output & insert proper numbers
 	annotations = annotations
-		//re-format commented underlines; lookahead ensures recognition of multi-line-comments in face of
-		//another underline, or document-end
+		// re-format commented underlines; lookahead ensures recognition of multi-line-comments in face of
+		// another underline, or document-end
 		.replace(
 			/ \* Page #?(\d+).*?:\n +> +(.*?)\n (.*?)(?=\n-|\n \*|\n$)/gs,
-			'- __$3:__ "$2" [' + pandoc_cite + 'p. $1]'
+			"- __$3:__ \"$2\" [" + pandocCite + "p. $1]"
 		)
-		//reformat multi-line-underlin comments properly
+		// reformat multi-line-underline comments properly
 		.replace(
 			/- __[^"]*\n[^"]*:__/gm,
-			function (ml){
+			function (ml) {
 				let output = "";
-				let lines = ml.slice(4,-3).split("\n");
-				lines.forEach(line =>{
+				const lines = ml.slice(4, -3).split("\n");
+				lines.forEach(line => {
 					output += "- __" + line.trim() + "__\n";
 				});
 				return output + "-";
 			}
 		)
-		//insert correct page numbers based on https://stackoverflow.com/a/32664436
+		// insert correct page numbers based on https://stackoverflow.com/a/32664436
 		.replace(
-		   /p\. (\d+)(?=\])/g,
-		   function (match, n) {
+			/p\. (\d+)(?=\])/g,
+			function (match, n) {
 				return "p. " + (parseInt(n) + firstPageNo);
 			}
 		);
 
-		// SPECIAL ANNOTATION CODES
+	// SPECIAL ANNOTATION CODES
 
-		// "+" and "++" (highlights)
-		// merge quotes with preceding quote
-		annotations = annotations
-			//"+": highlights on one page
-			.replace(
-				/" \[.*p\. (\d+)\]\n- __\+:__ "(.*?)" \[.*p\. /g,
-				" (…)" + ' $2" [' + pandoc_cite + 'p. $1-'
-			)
-			//"++": highlights spanning two page
-			.replace(
-				/" \[.*p\. (\d+)\]\n- __\+\+:__ "(.*?)" \[.*p\. /g,
-				' $2" [' + pandoc_cite + 'p. $1-'
-			)
-			//corrects page number for quotes merged on the same page
-			.replace(/p\. (\d+)-(\1)\]/g, "p. $1]");
+	// "+" and "++" (highlights)
+	// merge quotes with preceding quote
+	annotations = annotations
+	// "+": highlights on one page
+		.replace(
+			/" \[.*p\. (\d+)\]\n- __\+:__ "(.*?)" \[.*p\. /g,
+			" (…) $2\" [" + pandocCite + "p. $1-"
+		)
+	// "++": highlights spanning two page
+		.replace(
+			/" \[.*p\. (\d+)\]\n- __\+\+:__ "(.*?)" \[.*p\. /g,
+			" $2\" [" + pandocCite + "p. $1-"
+		)
+	// corrects page number for quotes merged on the same page
+		.replace(/p\. (\d+)-(\1)\]/g, "p. $1]");
 
 	return annotations;
 }
