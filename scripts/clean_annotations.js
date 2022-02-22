ceaec7943f2e85b4258f5ebd031587c7f65a1479
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-22 11:18:25 +0100

    heading transformation to titlecase whken it is all uppercase

--- scripts/clean_annotations.js
+++ scripts/clean_annotations.js
@@ -29,6 +29,18 @@ function run() {
 		});
 	}
 
+	String.prototype.toTitleCase = function () {
+		const smallWords = /\b(?:a[stn]?|and|because|but|by|en|for|i[fn]|neither|nor|o[fnr]|only|over|per|so|some|tha[tn]|the|to|up(on)?|vs?\.?|versus|via|when|with(out)?|yet)\b/i;
+		let capitalized = this.replace(/\w\S*/g, function(word) {
+			if (smallWords.test(word)) return word.toLowerCase();
+			if (word.toLowerCase() === "i") return "I";
+			if (word.length < 3) return word.toLowerCase();
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+		});
+		capitalized = capitalized.charAt(0).toUpperCase() + capitalized.slice(1).toLowerCase();
+		return capitalized;
+	};
+
 	// Core Methods
 	// --------------------------------------------------------------
 
@@ -237,7 +249,7 @@ function run() {
 			if (hLevel) {
 				if (a.type === "Highlight" || a.type === "Underline") {
 					let headingText = a.quote;
-					if (headingText === headingText.toUpperCase()) headingText = a.quote.charAt(0).toUpperCase() + a.quote.substr(1).toLowerCase();
+					if (headingText === headingText.toUpperCase()) headingText = headingText.toTitleCase();
 					a.comment = hLevel[0] + " " + headingText;
 					delete a.quote;
 				}
