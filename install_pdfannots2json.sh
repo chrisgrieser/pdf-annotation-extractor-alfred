6f3f9f61109d5f01898adee2405fd4d0b7e2bdc3
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-18 13:07:02 +0200

    patch

--- install_pdfannots2json.sh
+++ install_pdfannots2json.sh
@@ -7,8 +7,6 @@
 
 # --------------------------
 
-sudo -v
-
 if [[ $(uname -p) == "arm" ]]; then
 	PROCESSOR="M1"
 else
@@ -18,5 +16,5 @@ fi
 curl -OL "https://github.com/mgmeyers/pdf-annots2json/releases/latest/download/pdf-annots2json.Mac.$PROCESSOR.tar.gz"
 tar -xjf pdf-annots2json.Mac.M1.tar.gz || exit 1
 rm -f pdf-annots2json.Mac.M1.tar.gz
-mv -f -v pdf-annots2json /usr/local/bin/
+sudo mv -f -v pdf-annots2json /usr/local/bin/
 pdf-annots2json --version
