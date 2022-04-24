cc7a0114b20333c89dfbea190b7d0aea644a49b5
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-24 11:45:55 +0200

    patch

--- scripts/read-bibtex-library.js
+++ scripts/read-bibtex-library.js
@@ -89,7 +89,7 @@ function run() {
 	const array = bibtexEntry.split("\r");
 	array.forEach(property => {
 
-		if (/\stitle =/i.test(property)) title = extract(property);
+		if (/\stitle =/i.test(property)) title = extract(property).replaceAll("\"", "'"); // to avoid invalid yaml, since title is wrapped in ""
 		else if (property.includes("@")) ptype = property.replace(/@(.*)\{.*/, "$1");
 		else if (property.includes("pages =")) firstPage = property.match(/\d+/)[0];
 		else if (property.includes("author =")) author = extract(property);
