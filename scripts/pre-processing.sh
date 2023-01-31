72eb20b8ecf325da75a4617ffbdcdbf21c1f1691
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-01-31 01:22:59 +0100

    chore

--- scripts/pre-processing.sh
+++ scripts/pre-processing.sh
@@ -2,13 +2,9 @@
 
 pdf_path=$(osascript "./scripts/get-pdf-path.applescript")
 if [[ ! "$pdf_path" == *.pdf ]]; then
-	osascript -e "\"display notification \"\" with title \"$pdf_path is not a .pdf file.\""
+	echo -n "Error: $pdf_path is not a .pdf file."
 	exit 1	
 fi
 
 metadata=$(osascript -l JavaScript "./scripts/read-bibtex-library.js" "$pdf_path")
-if [[ "$metadata" == Error:* ]]; then
-	osascript -e "\"display notification \"\" with title \"$pdf_path is not a .pdf file.\""
-	exit 1	
-fi
 echo -n "$metadata"
